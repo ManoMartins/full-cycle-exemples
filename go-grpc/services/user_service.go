0f965b4150119ec1d2e2e57be3acfd9e15a4c2e5
Manoel Martins <mano.martins29@gmail.com>
2022-08-08 22:48:16 -0300

    :sparkles: implement service to receive stream

--- go-grpc/services/user_service.go
+++ go-grpc/services/user_service.go
@@ -2,7 +2,10 @@ package services
 
 import (
 	"context"
+	"fmt"
 	"go-grpc/pb"
+	"io"
+	"log"
 	"time"
 )
 
@@ -60,4 +63,30 @@ func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVe
 	time.Sleep(time.Second * 3)
 
 	return nil
+}
+
+func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
+	users := []*pb.User{}
+
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.Users{
+				User: users,
+			})
+		}
+
+		if err != nil {
+			log.Fatalf("Could not receive stream: %v", err)
+		}
+
+		users = append(users, &pb.User{
+			Id:    req.GetId(),
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		})
+
+		fmt.Println("Adding", req.GetName())
+	}
 }
