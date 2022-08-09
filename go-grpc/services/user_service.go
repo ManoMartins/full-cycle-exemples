ae0fc26b44df9942885d274d0e96399c46ec4f6d
Manoel Martins <mano.martins29@gmail.com>
2022-08-08 22:35:43 -0300

    :sparkles: implement server stream

--- go-grpc/services/user_service.go
+++ go-grpc/services/user_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"go-grpc/pb"
+	"time"
 )
 
 type UserService struct {
@@ -19,4 +20,44 @@ func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error)
 		Name:  req.GetName(),
 		Email: req.GetEmail(),
 	}, nil
+}
+
+func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+	stream.Send(&pb.UserResultStream{
+		Status: "Init",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Inserting",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "User has been inserted",
+		User: &pb.User{
+			Id:    "1",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Completed",
+		User: &pb.User{
+			Id:    "1",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	return nil
 }
