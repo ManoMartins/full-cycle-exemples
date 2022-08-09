a83bedf400b9ca977b1b77237fc383b7b4e193e9
Manoel Martins <mano.martins29@gmail.com>
2022-08-08 23:02:17 -0300

    :sparkles: implement service stream bi direcional

--- go-grpc/services/user_service.go
+++ go-grpc/services/user_service.go
@@ -90,3 +90,30 @@ func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
 		fmt.Println("Adding", req.GetName())
 	}
 }
+
+func (*UserService) AddUserStreamBoth(stream pb.UserService_AddUserStreamBothServer) error {
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			log.Fatalf("Could not receive stream: %v", err)
+		}
+
+		err = stream.Send(&pb.UserResultStream{
+			Status: "Inserting",
+			User:   req,
+		})
+
+		if err != nil {
+			log.Fatalf("Could not send stream: %v", err)
+		}
+
+		time.Sleep(time.Second * 3)
+	}
+
+	return nil
+}
